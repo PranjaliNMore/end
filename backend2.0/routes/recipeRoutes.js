175fbb6eee80849165d1addc58fa61c55c495b91
Pranjali <pranjali.n.more@gmail.com>
2025-03-10 14:15:08 +0530

    reciperoutes changes

--- backend2.0/routes/recipeRoutes.js
+++ backend2.0/routes/recipeRoutes.js
@@ -1,12 +1,12 @@
 const express = require("express");
-const { getRecipes, getMyRecipes } = require("../controllers/recipeController");
+const { getRecipes, getMyRecipes,CreateRecipe} = require("../controllers/recipeController");
 const authMiddleware = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
 router.get("/", getRecipes); // ✅ Get all recipes
 router.get("/my", authMiddleware, getMyRecipes); // ✅ Get logged-in user's recipes
-router.post("/", authMiddleware, createRecipe); // ✅ Add this route to create a recipe
+router.post("/", authMiddleware, CreateRecipe); // ✅ Add this route to create a recipe
 module.exports = router;
 
 
